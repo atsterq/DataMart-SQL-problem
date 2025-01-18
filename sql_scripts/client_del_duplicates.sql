8abbf2377f71868a02a2770abfc0513e7dcb48be
atsterq <atsterq@gmail.com>
2025-01-18 19:02:56 +0500

    task2.1 complete

--- sql_scripts/client_del_duplicates.sql
+++ sql_scripts/client_del_duplicates.sql
@@ -1,11 +1,20 @@
-select count(1) from dm.account_balance_turnover as abt ;
-select count(1) from dm.client as c ; -- 20147
-select count(1) from dm.dict_currency as dc ;
-select count(1) from dm.loan_holiday_info as lhi ; 
+-- УДАЛЕНИЕ ДУБЛИКАТОВ В dm.client по уникальному составному ключу client_rk , effective_from_date
+select count(1) from dm.client as c ; -- до удаленгия дубликатов 20147, 3299 после
 
-
-select client_rk, effective_from_date, count(*) as dup
-from dm.client
-group by client_rk, effective_from_date
-having count(*) > 1;
-
+begin;
+with dup_t as (
+	select 
+		row_number () over (partition by client_rk, effective_from_date order by effective_from_date desc) as row_num
+		, client_rk , effective_from_date 
+	from dm.client as c
+)
+delete 
+	from
+		dm.client as c2 
+	where 
+		(c2.client_rk, c2.effective_from_date) in (
+		select d.client_rk, d.effective_from_date 
+		from dup_t d
+		where row_num > 1);
+rollback; -- на всякий случай оставил возможность откатиться
+end;
