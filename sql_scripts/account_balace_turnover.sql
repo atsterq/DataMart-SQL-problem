e80567ec85d5b02d2247782a9c9e52e13601e336
atsterq <atsterq@gmail.com>
2025-01-24 17:43:21 +0500

    task2.3 update

---
+++ sql_scripts/account_balace_turnover.sql
@@ -0,0 +1,16 @@
+--2.3 dm.account_balance_turnover
+-- прототип витрины
+SELECT a.account_rk,
+	   COALESCE(dc.currency_name, '-1'::TEXT) AS currency_name,
+	   a.department_rk,
+	   ab.effective_date,
+	   ab.account_in_sum,
+	   ab.account_out_sum
+FROM rd.account a
+LEFT JOIN rd.account_balance ab ON a.account_rk = ab.account_rk
+LEFT JOIN dm.dict_currency dc ON a.currency_cd = dc.currency_cd;
+
+
+--анализ
+select * from dm.account_balance_turnover as abt ;
+select * from 
